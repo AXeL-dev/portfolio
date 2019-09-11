a0116592ee81b23119265fc076f5cf047b305dc9
AXeL-dev <contact.axel.dev@gmail.com>
2019-09-11 10:10:48 +0100

    Removed custom.js file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fadeOutAnimation } from './animations/fade-out.animation';
 
+declare var M: any;
 declare var imagesLoaded: any;
 
 @Component({
@@ -14,6 +15,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     let _self = this;
+    // Initialize Materialize tooltips
+    document.addEventListener('DOMContentLoaded', function () {
+      const tooltips = document.querySelectorAll('.tooltipped');
+      let instances = M.Tooltip.init(tooltips);
+    });
     // Wait for background images to load before removing the preloader
     const images = document.querySelectorAll('.image-bg');
     imagesLoaded(images, { background: true }, function () {
--- src/assets/js/custom.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	// Initialize tooltips
-	var elems = document.querySelectorAll('.tooltipped');
-	var instances = M.Tooltip.init(elems);
-});
