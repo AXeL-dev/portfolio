a54e2eff0200cba0c27f00d6a764e164d3977ce8
AXeL-dev <contact.axel.dev@gmail.com>
2019-09-11 21:01:37 +0100

    Update carousel-section.component.ts
    Make carousel responsive

--- src/app/components/sections/carousel-section/carousel-section.component.ts
+++ src/app/components/sections/carousel-section/carousel-section.component.ts
@@ -15,7 +15,21 @@ export class CarouselSectionComponent implements OnInit {
     items: 5,
     dots: false,
     autoplay: true,
-    autoplaySpeed: 500
+    autoplaySpeed: 500,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    }
   };
 
   constructor() { }
