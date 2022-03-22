166109c5229b7a427b59219c4d099cbac9d17db7
AXeL-dev <contact.axel.dev@gmail.com>
2022-03-22 11:57:47 +0100

    fix: cleanup tooltips styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { fadeOutAnimation } from './animations/fade-out.animation';
 import { Router, UrlTree } from '@angular/router';
 import { DisqusConfig } from './services/blog.service';
+import { DebugService } from './services/debug.service';
 import { environment } from 'src/environments/environment';
 
 declare var M: any;
@@ -11,7 +12,7 @@ declare var imagesLoaded: any;
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [fadeOutAnimation]
+  animations: [fadeOutAnimation],
 })
 export class AppComponent implements OnInit {
 
@@ -19,7 +20,7 @@ export class AppComponent implements OnInit {
   isHomePage: boolean = false;
   private route: UrlTree = null;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private debug: DebugService) { }
 
   ngOnInit() {
     // document.addEventListener('DOMContentLoaded', () => {
@@ -30,17 +31,28 @@ export class AppComponent implements OnInit {
   }
 
   initTooltips() {
-    //console.log('Init tooltips');
+    this.debug.log('Init tooltips');
     // Initialize Materialize tooltips
     const tooltips = document.querySelectorAll('.tooltipped');
-    let instances = M.Tooltip.init(tooltips);
+    const instances = M.Tooltip.init(tooltips);
+    // Cleanup tooltips styles on hide
+    for (const tooltip of instances) {
+      const { _animateOut } = tooltip;
+      tooltip._animateOut = () => {
+        _animateOut.apply(tooltip);
+        setTimeout(() => {
+          tooltip.tooltipEl.style.removeProperty('top');
+          tooltip.tooltipEl.style.removeProperty('left');
+        }, tooltip.options.outDuration);
+      };
+    }
   }
 
   disablePreloader(scroll: boolean = false, then: Function = () => {}) {
-    //console.log('Disable preloader');
+    this.debug.log('Disable preloader');
     // Wait for background images to load before removing the preloader
     imagesLoaded('.image-bg', { background: true }, () => {
-      //console.log('all images are loaded');
+      this.debug.log('all images are loaded');
       this.isLoading = false;
       then();
       if (scroll) {
@@ -50,8 +62,11 @@ export class AppComponent implements OnInit {
   }
 
   private scrollToRouteFragment() {
-    //console.log('Scroll to route fragment');
-    const element = this.route && this.route.fragment ? document.getElementById(this.route.fragment) : null;
+    this.debug.log('Scroll to route fragment');
+    const element =
+      this.route && this.route.fragment
+        ? document.getElementById(this.route.fragment)
+        : null;
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
@@ -79,6 +94,8 @@ export class AppComponent implements OnInit {
   onRouteChange() {
     this.isLoading = true;
     this.route = this.router.parseUrl(this.router.url);
-    this.isHomePage = !this.route.root.hasChildren() || this.route.root.children.primary.segments[0].path === 'home';
+    this.isHomePage =
+      !this.route.root.hasChildren() ||
+      this.route.root.children.primary.segments[0].path === 'home';
   }
 }
